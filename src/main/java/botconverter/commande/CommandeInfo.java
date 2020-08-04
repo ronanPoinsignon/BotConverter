4c6de7d78bc72daabcca99cb41469ca9c0f0ad51
ronanPoinsignon <ronan.poinsignon@laposte.net>
2020-08-04 04:47:38 +0200

    mise des commandes dans des threads

--- src/main/java/botconverter/commande/CommandeInfo.java
+++ src/main/java/botconverter/commande/CommandeInfo.java
@@ -11,13 +11,25 @@ public class CommandeInfo extends ListenerAdapter {
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
 		if(args[0].equals(Bot.PREFIX + "info")) {
-			EmbedBuilder embed = new EmbedBuilder();
-			embed.setTitle("Liste des commandes");
-			embed.addField("Commande Info", "Donne la liste des commandes disponibles.\nex : "
-					+ Bot.PREFIX + "info", false);
-			embed.addField("Commande mp3", "Convertit une vidéo Youtube en fichier mp3.\nex : " 
-					+ Bot.PREFIX + "mp3 https://www.youtube.com/watch?v=hT_nvWreIhg", false);
-			event.getChannel().sendMessage(embed.build()).queue();
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					EmbedBuilder embed = new EmbedBuilder();
+					embed.setTitle("Liste des commandes");
+					embed.addField("Commande Info", "Donne la liste des commandes disponibles.\nex : "
+							+ Bot.PREFIX + "info", false);
+					embed.addField("Commande mp3", "Convertit une vidéo Youtube en fichier mp3.\nex : " 
+							+ Bot.PREFIX + "mp3 https://www.youtube.com/watch?v=hT_nvWreIhg", false);
+					event.getChannel().sendMessage(embed.build()).queue();
+					try {
+						Thread.currentThread().interrupt();
+					}
+					catch(SecurityException e) {
+						
+					}
+				}
+			}).start();
 		}
 	}
 	
--- src/main/java/botconverter/commande/CommandeMp3.java
+++ src/main/java/botconverter/commande/CommandeMp3.java
@@ -19,70 +19,87 @@ public class CommandeMp3 extends ListenerAdapter {
 
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
 		if(args[0].equals(Bot.PREFIX + "mp3")) {
-			if(args.length != 2) {
-				EmbedBuilder embed = new EmbedBuilder();
-				embed.setTitle("Erreur dans la commande");
-				embed.setColor(Color.RED);
-				embed.setDescription("la commande ne doit avoir qu'un paramètre ");
-				return;
-			}
-			String url = args[1];
-			List<String> listeExtensions = new ArrayList<>();
-			listeExtensions.add("mp3");
-			TacheConvertirInstant tache = new TacheConvertirInstant(url, BOT_FOLDER, BIT_RATE, listeExtensions);
-			List<File> listeMp3 = tache.convertir();
-			List<Video> listeMauvaisesVideos = tache.getListeMauvaisesVideos();
-			List<String> listeMauvaisLiens = tache.getListeMauvaisLiens();
-			List<String> listeUrlsErreur = tache.getListeUrlsErreur();
-			
-			if(!listeMp3.isEmpty()) {
-				try {
-					event.getChannel().sendFile(listeMp3.get(0)).queue();
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					if(args.length != 2) {
+						EmbedBuilder embed = new EmbedBuilder();
+						embed.setTitle("Erreur dans la commande");
+						embed.setColor(Color.RED);
+						embed.setDescription("la commande ne doit avoir qu'un paramètre ");
+						event.getChannel().sendMessage(embed.build()).queue();
+						return;
+					}
+					String url = args[1];
+					List<String> listeExtensions = new ArrayList<>();
+					listeExtensions.add("mp3");
+					TacheConvertirInstant tache = new TacheConvertirInstant(url, BOT_FOLDER, BIT_RATE, listeExtensions);
+					List<File> listeMp3 = tache.convertir();
+					List<Video> listeMauvaisesVideos = tache.getListeMauvaisesVideos();
+					List<String> listeMauvaisLiens = tache.getListeMauvaisLiens();
+					List<String> listeUrlsErreur = tache.getListeUrlsErreur();
+					
+					if(!listeMp3.isEmpty()) {
+						try {
+							event.getChannel().sendFile(listeMp3.get(0)).queue();
+						}
+						catch(IllegalArgumentException e) {
+							if(e.getMessage().startsWith("File may not exceed the maximum file length")) {
+								EmbedBuilder embed = new EmbedBuilder();
+								embed.setTitle("Fichier trop volumineux");
+								embed.setDescription("Le fichier est trop important pour être envoyé (taille du fichier : " + listeMp3.get(0).length()/1000 + "Mo");
+								event.getChannel().sendMessage(embed.build()).queue();
+							}
+						}
+					}
+					
+					for(File fichier : listeMp3) {
+						fichier.delete();
+					}
+					
+					if(!listeMauvaisesVideos.isEmpty()) {
+						EmbedBuilder embed = new EmbedBuilder();
+						embed.setTitle("Vidéo(s) non valide(s)");
+						embed.setColor(Color.RED);
+						StringBuilder str = new StringBuilder("La ou les vidéos suivantes ne sont pas valides :");
+						for(Video video : listeMauvaisesVideos)
+							str.append("\n\t- " + video.getLien());
+						embed.setDescription(str.toString());
+						event.getChannel().sendMessage(embed.build()).queue();
+					}
+					
+					if(!listeMauvaisLiens.isEmpty()) {
+						EmbedBuilder embed = new EmbedBuilder();
+						embed.setTitle("Lien(s) non valide(s)");
+						embed.setColor(Color.RED);
+						StringBuilder str = new StringBuilder("Le ou les liens suivants ne redirigent pas vers une vidéo Youtube :");
+						for(String lien : listeMauvaisLiens)
+							str.append("\n\t- " + lien);
+						embed.setDescription(str.toString());
+						event.getChannel().sendMessage(embed.build()).queue();;
+					}
+					
+					if(!listeUrlsErreur.isEmpty()) {
+						EmbedBuilder embed = new EmbedBuilder();
+						embed.setTitle("Lien(s) non valide(s)");
+						embed.setColor(Color.RED);
+						StringBuilder str = new StringBuilder("le ou les liens suivants ont apporté une erreur à l'exécution :");
+						for(String lien : listeUrlsErreur)
+							str.append("\n\t- " + lien);
+						embed.setDescription(str.toString());
+						event.getChannel().sendMessage(embed.build()).queue();
+					}
+					try {
+						Thread.currentThread().interrupt();
+					}
+					catch(SecurityException e) {
+						
+					}
 				}
-				catch(IllegalArgumentException e) {
-					System.out.println(e.getMessage());
-					System.out.println(listeMp3.get(0).length());
-				}
-			}
-			
-			for(File fichier : listeMp3) {
-				fichier.delete();
-			}
-			
-			if(!listeMauvaisesVideos.isEmpty()) {
-				EmbedBuilder embed = new EmbedBuilder();
-				embed.setTitle("Vidéo(s) non valide(s)");
-				embed.setColor(Color.RED);
-				StringBuilder str = new StringBuilder("La ou les vidéos suivantes ne sont pas valides :");
-				for(Video video : listeMauvaisesVideos)
-					str.append("\n\t- " + video.getLien());
-				embed.setDescription(str.toString());
-				event.getChannel().sendMessage(embed.build()).queue();
-			}
-			
-			if(!listeMauvaisLiens.isEmpty()) {
-				EmbedBuilder embed = new EmbedBuilder();
-				embed.setTitle("Lien(s) non valide(s)");
-				embed.setColor(Color.RED);
-				StringBuilder str = new StringBuilder("Le ou les liens suivants ne redirigent pas vers une vidéo Youtube :");
-				for(String lien : listeMauvaisLiens)
-					str.append("\n\t- " + lien);
-				embed.setDescription(str.toString());
-				event.getChannel().sendMessage(embed.build()).queue();;
-			}
-			
-			if(!listeUrlsErreur.isEmpty()) {
-				EmbedBuilder embed = new EmbedBuilder();
-				embed.setTitle("Lien(s) non valide(s)");
-				embed.setColor(Color.RED);
-				StringBuilder str = new StringBuilder("le ou les liens suivants ont apporté une erreur à l'exécution :");
-				for(String lien : listeUrlsErreur)
-					str.append("\n\t- " + lien);
-				embed.setDescription(str.toString());
-				event.getChannel().sendMessage(embed.build()).queue();
-			}
+			}).start();
 		}
 	}
 	
